2bb9e8f12be262ab4fff2e18c4f3735284057401
vitacho <victor.a.jimenez@unl.edu.ec>
2023-11-07 09:40:39 -0500

    Cambios en requirements.txt actulizacion de librerias por seguridad

--- pwtea/pwtea/settings.py
+++ pwtea/pwtea/settings.py
@@ -57,7 +57,8 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'corsheaders',
     #coreapi
-    'coreapi',
+    'coreapi'
+    #cloudinary
 
 
 ]
@@ -171,6 +172,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
